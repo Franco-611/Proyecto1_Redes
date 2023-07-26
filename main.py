f514ce7d781e5dc27623df32d042311ade94e9f7
Franco-611 <69815571+Franco-611@users.noreply.github.com>
2023-07-26 08:43:04 -0600

    Creacion: - Funcion para interactuar con el cliente luego de iniciar sesion

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def registrar_cuenta(jid, password):
 def iniciar_sesion(jid, password):
     xmpp = XMPPClient(jid, password)
     xmpp.use_tls = False 
-    xmpp.connect(address=('alumchat.xyz', 5222))  # Establecer la conexión con el servidor
+    xmpp.connect(address=('alumchat.xyz', 5222)) 
 
     if xmpp.connect():
 
@@ -34,11 +34,9 @@ def iniciar_sesion(jid, password):
 
         print("Sesión iniciada correctamente.")
 
-        print("Comandos disponibles:")
-        print("1. show_contacts() -> Mostrar todos los contactos y su estado.")
-        print("2. add_contact(jid) -> Agregar un usuario a los contactos.")
-        print("3. contact_details(jid) -> Mostrar detalles de contacto de un usuario.")
-        print("4. send_message(to_jid, message) -> Enviar un mensaje a un usuario.")
+        interactuar_con_cliente(xmpp)
+
+    
 
         while True:
             try:
@@ -52,6 +50,37 @@ def iniciar_sesion(jid, password):
         xmpp.disconnect()
     else:
         print("Error en la conexión. No se pudo iniciar sesión.")
+
+def interactuar_con_cliente(xmpp):
+
+    print("Comandos disponibles:")
+    print("1. show_contacts() -> Mostrar todos los contactos y su estado.")
+    print("2. add_contact(jid) -> Agregar un usuario a los contactos.")
+    print("3. contact_details(jid) -> Mostrar detalles de contacto de un usuario.")
+    print("4. send_message(to_jid, message) -> Enviar un mensaje a un usuario.")
+
+    while True:
+        try:
+            opcion = input("Ingrese el número de la opción (o '5' para salir): ")
+            if opcion == '1':
+                xmpp.show_contacts()
+            elif opcion == '2':
+                jid = input("Ingrese el JID del usuario que desea agregar: ")
+                xmpp.add_contact(jid)
+            elif opcion == '3':
+                jid = input("Ingrese el JID del usuario del que desea ver detalles: ")
+                xmpp.contact_details(jid)
+            elif opcion == '4':
+                jid = input("Ingrese el JID del usuario al que desea enviar el mensaje: ")
+                message = input("Ingrese el mensaje: ")
+                xmpp.send_message(jid, message)
+            elif opcion == '5':
+                break
+            else:
+                print("Opción inválida. Por favor, ingrese un número válido.")
+        except Exception as e:
+            print(f"Error: {e}")
+
 
 if __name__ == "__main__":
     print("Bienvenido al cliente de mensajería XMPP")
