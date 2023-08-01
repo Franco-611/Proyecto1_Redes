0797f5307a5b258652476e0ae985a50ded1e6fc8
Franco-611 <69815571+Franco-611@users.noreply.github.com>
2023-08-01 11:40:15 -0600

    Inicio de sesion

--- cliente.py
+++ cliente.py
@@ -1,21 +1,53 @@
 import slixmpp
+from aioconsole import ainput
+import asyncio
 
 class MyCliente(slixmpp.ClientXMPP):
     def __init__(self, jid, password):
         super().__init__(jid, password)
+        self.conectado = False
 
-        self.register_plugin('xep_0030') # Service Discovery
-        self.register_plugin('xep_0199') # Ping
-        self.register_plugin('xep_0045') # MUC
-        self.register_plugin('xep_0085') # Notifications
-        self.register_plugin('xep_0004') # Data Forms
-        self.register_plugin('xep_0060') # PubSub
 
         self.add_event_handler("session_start", self.start)
 
     def start(self, event):
         self.send_presence()
         self.get_roster()
+        self.conectado = True
+        print("Sesión iniciada correctamente.")
+        asyncio.create_task(self.interactuar_con_cliente())
+
+    async def interactuar_con_cliente(self):
+
+        print("Comandos disponibles:")
+        print("1. show_contacts() -> Mostrar todos los contactos y su estado.")
+        print("2. add_contact(jid) -> Agregar un usuario a los contactos.")
+        print("3. contact_details(jid) -> Mostrar detalles de contacto de un usuario.")
+        print("4. send_message(to_jid, message) -> Enviar un mensaje a un usuario.")
+        print("5. exit() -> Salir del programa.")
+
+        while self.conectado:
+            try:
+                opcion = await ainput("Ingrese el número de la opción (o '5' para salir): ")
+                if opcion == '1':
+                    self.show_contacts()
+                elif opcion == '2':
+                    jid = input("Ingrese el JID del usuario que desea agregar: ")
+                    self.add_contact(jid)
+                elif opcion == '3':
+                    jid = input("Ingrese el JID del usuario del que desea ver detalles: ")
+                    self.contact_details(jid)
+                elif opcion == '4':
+                    jid = input("Ingrese el JID del usuario al que desea enviar el mensaje: ")
+                    message = input("Ingrese el mensaje: ")
+                    self.send_message(jid, message)
+                elif opcion == '5':
+                    self.conectado = False
+                    self.disconnect()
+                else:
+                    print("Opción inválida. Por favor, ingrese un número válido.")
+            except Exception as e:
+                print(f"Error: {e}")
 
     def show_contacts(self):
         contacts = self.client_roster.keys()
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cliente import MyCliente
 import xmpp
+import asyncio
 
 def registrar_cuenta(jid, password):
     
@@ -19,61 +20,11 @@ def iniciar_sesion(jid, password):
     xmpp.connect(disable_starttls=True)
     xmpp.process(forever=False)
 
-    #     while not xmpp.state.ensure('session_started'):
-    #         pass
-
-    #     print("Sesión iniciada correctamente.")
-
-    #     interactuar_con_cliente(xmpp)
-
-    
-
-    #     while True:
-    #         try:
-    #             command = input("Ingrese el comando (o 'exit' para salir): ")
-    #             if command.lower() == 'exit':
-    #                 break
-    #             else:
-    #                 eval(command)
-    #         except Exception as e:
-    #             print(f"Error: {e}")
-    #     xmpp.disconnect()
-    # else:
-    #     print("Error en la conexión. No se pudo iniciar sesión.")
-
-def interactuar_con_cliente(xmpp):
-
-    print("Comandos disponibles:")
-    print("1. show_contacts() -> Mostrar todos los contactos y su estado.")
-    print("2. add_contact(jid) -> Agregar un usuario a los contactos.")
-    print("3. contact_details(jid) -> Mostrar detalles de contacto de un usuario.")
-    print("4. send_message(to_jid, message) -> Enviar un mensaje a un usuario.")
-
-    while True:
-        try:
-            opcion = input("Ingrese el número de la opción (o '5' para salir): ")
-            if opcion == '1':
-                xmpp.show_contacts()
-            elif opcion == '2':
-                jid = input("Ingrese el JID del usuario que desea agregar: ")
-                xmpp.add_contact(jid)
-            elif opcion == '3':
-                jid = input("Ingrese el JID del usuario del que desea ver detalles: ")
-                xmpp.contact_details(jid)
-            elif opcion == '4':
-                jid = input("Ingrese el JID del usuario al que desea enviar el mensaje: ")
-                message = input("Ingrese el mensaje: ")
-                xmpp.send_message(jid, message)
-            elif opcion == '5':
-                break
-            else:
-                print("Opción inválida. Por favor, ingrese un número válido.")
-        except Exception as e:
-            print(f"Error: {e}")
 
 
 if __name__ == "__main__":
     print("Bienvenido al cliente de mensajería XMPP")
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
     while True:
         choice = input("¿Que deseas realizar? \n  (1) Crear un nuevo usuario \n  (2) Iniciar sesión con uno existente  \n  (3) Salir \n  -> ")
